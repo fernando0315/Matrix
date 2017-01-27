bc4570750e28d044ab57e854973c095a39bf17f9
Gerry Fernando Patia <fernando_nxt@hotmail.com>
2017-01-26 23:48:27 -0800

    testing file finalize

--- main.cpp
+++ main.cpp
@@ -5,29 +5,53 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <iomanip>
 #include "matrix.h"
 
 using namespace std;
 
-bool getMatrixFromFile(Matrix&);
+bool getMatrixFromFile(Matrix&, double*&);
 
 
 int main() 
 {
 	Matrix mat;
-	getMatrixFromFile(mat); 
+	double *invMatrix, *x, *b;
+
+	//Get data from file
+	if(!getMatrixFromFile(mat, b)) return 1;
+
+	//Creat Matrix and LU Matrix
 	cout << mat;
 	mat.factor();
 	mat.printUMatrix();
 	mat.printLMatrix();
 	cout << "Determinant: " << mat.findDet() << endl;
-	double b[] = {500, 0, 0, 0};
-	mat.solve(b);
+	
+	//Solver for x for Ax = B
+	cout << "Ax = B, B:" << endl;
+	for(int j=0;j < 4; j++)
+		cout << setw(6) << right << fixed << setprecision(2) << b[j] << endl;
+	cout << "value of x: " << endl;
+	x = mat.solve(b);
+	for(int j=0;j < 4; j++)
+		cout << setw(7) << right <<setprecision(2) << x[j] << endl;
+
+	//Solver for inverse matrix
+	cout << "Inverse Matrix: " << endl;
+	invMatrix = mat.findInv();
+	int mval=mat.getRow();
+	int nval=mat.getCol();
+	for (int i=0; i<mval;i++){
+		for(int j=0;j < nval ; j++)
+			cout << setw(7) << right << fixed << setprecision(4) << invMatrix[i*nval+j] << ' ';
+		cout << endl;
+	}
 
 	return 0;
 }
 
-bool getMatrixFromFile(Matrix &mat) 
+bool getMatrixFromFile(Matrix &mat, double *&B) 
 {
 	string fname;
 	cout << "Input File name:";
@@ -37,8 +61,8 @@ bool getMatrixFromFile(Matrix &mat)
 
 	if(!ifile)
 	{
-		cout << "File not found" << endl;
-		return NULL;
+		cout << "File not found!!!" << endl;
+		return false;
 	}
 
 	int nRows, nCols, val;
@@ -52,8 +76,14 @@ bool getMatrixFromFile(Matrix &mat)
 			ifile >> val;
 			a(i,j) = val;
 		}
+	
+	B = new double[nRows];
+	for(int i = 0; i < nRows; i++)
+		ifile >> B[i];
 
 	mat = a;
 
 	ifile.close();   // close the diskfile
+
+	return true;
 }
